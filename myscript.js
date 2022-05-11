9ef798da60056919453bf9a0cb61e26514ddb12d
hannahblankenship <101372932+hannahblankenship@users.noreply.github.com>
2022-05-10 22:48:07 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $('.wd_am').hover(function(){
-      $('.skill').css('background-color','yellow');
+      $('.skill').css('background-color','cyan');
   },
                     function(){
     $('.skill').css('background-color','magenta');  
