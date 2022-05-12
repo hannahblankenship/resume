686b9c88ea02cfe585cddff2dd7f7656bd9232eb
hannahblankenship <101372932+hannahblankenship@users.noreply.github.com>
2022-05-11 17:08:53 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -17,14 +17,14 @@ $(document).ready(function(){
   $('.ex2').hover(function(){
     $('#ssc').css('background-color','magenta');
     $('#ssl').css('background-color','magenta');
-    $('#sspro').css('background-color','magenta');
+    $('#sspm').css('background-color','magenta');
     $('#swm').css('background-color','magenta');
     $('#sws').css('background-color','magenta');
   },
   function(){
     $('#ssc').css('background-color','transparent');
     $('#ssl').css('background-color','transparent');
-    $('#sspro').css('background-color','transparent');
+    $('#sspm').css('background-color','transparent');
     $('#swm').css('background-color','transparent');
     $('#sws').css('background-color','transparent');
   });
@@ -32,24 +32,24 @@ $(document).ready(function(){
     /* EX 3 */
   $('.ex3').hover(function(){
     $('#ssl').css('background-color','magenta');
-    $('#sspre').css('background-color','magenta');
+    $('#ssp').css('background-color','magenta');
     $('#swm').css('background-color','magenta');
       },
   function(){
     $('#ssl').css('background-color','transparent');
-    $('#sspre').css('background-color','transparent');
+    $('#ssp').css('background-color','transparent');
     $('#swm').css('background-color','transparent');
   });
   
       /* EX 4 */
   $('.ex4').hover(function(){
     $('#ssl').css('background-color','magenta');
-    $('#sspre').css('background-color','magenta');
+    $('#ssp').css('background-color','magenta');
     $('#swm').css('background-color','magenta');
       },
   function(){
     $('#ssl').css('background-color','transparent');
-    $('#sspre').css('background-color','transparent');
+    $('#ssp').css('background-color','transparent');
     $('#swm').css('background-color','transparent');
   });
   
