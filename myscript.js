b0d7644e4bb3c2f4ae6e8417669e0b6161690258
hannahblankenship <101372932+hannahblankenship@users.noreply.github.com>
2022-05-12 13:40:50 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,64 +1,65 @@
 $(document).ready(function(){
   
-  var skill_color = 'green';
+  var skill_color = 'magenta';
+  var skill_clear = 'transparent;
   
   /* WD AM Experiences*/
   /* EX 1 */
   $('.ex1').hover(function(){
     $('#ssc').css('background-color', skill_color); /*communication highlight*/
-    $('#sspm').css('background-color','magenta'); /*project management highlight*/
-    $('#sst').css('background-color','magenta'); /*teamwork highlight*/
-    $('#swm').css('background-color','magenta'); /*microsoft office highlight*/
-    $('#sws').css('background-color','magenta'); /*salesforce highlight*/
+    $('#sspm').css('background-color', skill_color); /*project management highlight*/
+    $('#sst').css('background-color', skill_color); /*teamwork highlight*/
+    $('#swm').css('background-color', skill_color); /*microsoft office highlight*/
+    $('#sws').css('background-color', skill_color); /*salesforce highlight*/
   },
   function(){
-    $('#ssc').css('background-color','transparent'); /*communication clear*/
-    $('#sspm').css('background-color','transparent'); /*project management clear*/
-    $('#sst').css('background-color','transparent'); /*teamwork clear*/
-    $('#swm').css('background-color','transparent'); /*microsoft office clear*/
-    $('#sws').css('background-color','transparent'); /*salesforce clear*/
+    $('#ssc').css('background-color', skill_clear); /*communication clear*/
+    $('#sspm').css('background-color', skill_clear); /*project management clear*/
+    $('#sst').css('background-color', skill_clear); /*teamwork clear*/
+    $('#swm').css('background-color', skill_clear); /*microsoft office clear*/
+    $('#sws').css('background-color', skill_clear); /*salesforce clear*/
   });
   
   /* EX 2 */
   $('.ex2').hover(function(){
-    $('#ssc').css('background-color','magenta'); /*communication highlight*/
-    $('#ssl').css('background-color','magenta'); /*leadership highlight*/
-    $('#sspm').css('background-color','magenta'); /*project management highlight*/
-    $('#sst').css('background-color','magenta'); /*teamwork highlight*/
-    $('#swm').css('background-color','magenta'); /*microsoft office highlight*/
-    $('#sws').css('background-color','magenta'); /*salesforce highlight*/
+    $('#ssc').css('background-color', skill_color); /*communication highlight*/
+    $('#ssl').css('background-color', skill_color); /*leadership highlight*/
+    $('#sspm').css('background-color', skill_color); /*project management highlight*/
+    $('#sst').css('background-color', skill_color); /*teamwork highlight*/
+    $('#swm').css('background-color', skill_color); /*microsoft office highlight*/
+    $('#sws').css('background-color', skill_color); /*salesforce highlight*/
   },
   function(){
-    $('#ssc').css('background-color','transparent'); /*communication clear*/
-    $('#ssl').css('background-color','transparent'); /*leadership clear*/
-    $('#sspm').css('background-color','transparent'); /*project management clear*/
-    $('#sst').css('background-color','transparent'); /*teamwork clear*/
-    $('#swm').css('background-color','transparent'); /*microsoft office clear*/
-    $('#sws').css('background-color','transparent'); /*salesforce clear*/
+    $('#ssc').css('background-color', skill_clear); /*communication clear*/
+    $('#ssl').css('background-color', skill_clear); /*leadership clear*/
+    $('#sspm').css('background-color', skill_clear); /*project management clear*/
+    $('#sst').css('background-color', skill_clear); /*teamwork clear*/
+    $('#swm').css('background-color', skill_clear); /*microsoft office clear*/
+    $('#sws').css('background-color', skill_clear); /*salesforce clear*/
   });
   
   /* EX 3 */
   $('.ex3').hover(function(){
-    $('#ssl').css('background-color','magenta');
-    $('#ssp').css('background-color','magenta');
-    $('#swm').css('background-color','magenta');
+    $('#ssl').css('background-color', skill_color);
+    $('#ssp').css('background-color', skill_color);
+    $('#swm').css('background-color', skill_color);
       },
   function(){
-    $('#ssl').css('background-color','transparent');
-    $('#ssp').css('background-color','transparent');
-    $('#swm').css('background-color','transparent');
+    $('#ssl').css('background-color', skill_clear);
+    $('#ssp').css('background-color', skill_clear);
+    $('#swm').css('background-color', skill_clear);
   });
   
   /* EX 4 */
   $('.ex4').hover(function(){
-    $('#ssl').css('background-color','magenta');
-    $('#ssp').css('background-color','magenta');
-    $('#swm').css('background-color','magenta');
+    $('#ssl').css('background-color', skill_color);
+    $('#ssp').css('background-color', skill_color);
+    $('#swm').css('background-color', skill_color);
       },
   function(){
-    $('#ssl').css('background-color','transparent');
-    $('#ssp').css('background-color','transparent');
-    $('#swm').css('background-color','transparent');
+    $('#ssl').css('background-color', skill_clear);
+    $('#ssp').css('background-color', skill_clear);
+    $('#swm').css('background-color', skill_clear);
   });
   
 });
